36703e0ece76352006c6604b0110e1363c422b65
HightMaster <89708788+HightMaster@users.noreply.github.com>
2023-04-02 15:20:32 -0400

    Database configuration
    Database is now created and the relevant connection information has been updated in db.js. Program is still using mongodb instead of mongoose

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
-const ID=''; 
-const PASSWORD = ''; 
-const NET='';
+const ID='guest'; 
+const PASSWORD = '0u0FzL6WBaHnqtZh'; 
+const NET='asecourses.jsbbhi4.mongodb.net';
 
 // Connection URI
-const uri = `mongodb+srv://${ID}:${PASSWORD}@${NET}/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${ID}:${PASSWORD}@${NET}/test`
 
 module.exports = uri
