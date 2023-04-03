2daf8fcb83edbc86f5b713d0331770140d03ee76
HightMaster <89708788+HightMaster@users.noreply.github.com>
2023-04-02 21:59:37 -0400

    API and database are nearly done
    The only thing missing is api Updating the Counter colection. I'll edit this when I know that we need to use that collection in the first place

--- db.js
+++
@@ -1,8 +0,0 @@
-const ID='guest'; 
-const PASSWORD = '0u0FzL6WBaHnqtZh'; 
-const NET='asecourses.jsbbhi4.mongodb.net';
-
-// Connection URI
-const uri = `mongodb+srv://${ID}:${PASSWORD}@${NET}/test`
-
-module.exports = uri
--- index.js
+++ index.js
@@ -4,36 +4,39 @@
 // npm install method-override
 // nodemon ./index.js
 // Access this server with http://localhost:5500/pet or http://localhost:5500/
-
-const { MongoClient } = require('mongodb');
-
-const uri = require('./db.js');
 var db;
 
 const DATABASE = 'todoapp';
-const POSTS = 'posts';
+const TASKS = 'Tasks';
 const COUNTER = 'counter';
 
-MongoClient.connect(uri, { useUnifiedTopology: true }, function (error, client) {
-    if (error) return console.log(error)
-    db = client.db(DATABASE);
-});
-
-const express = require('express');
+//Updated variables for express, mongoose, and dotenv
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
+const dotenv = require("dotenv");
+dotenv.config();
+const { taskModel, counterModel } = require('./models/models.ejs');
+//const counterModel = require('./models/Counter.ejs')
+//const methodOverride = require('method-override')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs');
+//IMPORTANT: *Not sure what this does
 app.use('/public', express.static('public'));
-app.use(methodOverride('_method'))
+//app.use(methodOverride('_method'))
 
-app.listen(5500, function () {
-    console.log('listening on 5500')
-});
+//Code block to startup the web server
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(process.env.DB_CONNECT);
+  console.log("Connected to db!");
+  app.listen(5500, () => console.log("Server Up and running"));
+}
 
+//Homepage request
 app.get('/', function (req, resp) {
     try {
         resp.render('write.ejs')
@@ -49,43 +52,54 @@ app.post('/add', function (req, resp) {
 
 async function runAddPost(req, resp) {
     try {
-        const counter = db.collection(COUNTER);
-        const posts = db.collection(POSTS);
-
-        let query = { name: 'Total Post' };
-        let res = await counter.findOne(query);
-        console.log(res);
-        const totalPost = res.totalPost;
-
-        query = { _id: totalPost + 1, title: req.body.title, date: req.body.date };
-        res = await posts.insertOne(query);
-
-        query = { name: 'Total Post' };
-        let stage = { $inc: { totalPost: 1 } };
-        await counter.updateOne(query, stage);
+        //Random number is generated and set as the ID
+        let number = await (Math.floor(Math.random() * 200000));
+        const task = await new taskModel({ taskID: number, title: req.body.title, date: req.body.date })
+        //Saves new task to the Database
+        await task.save();
     } catch (e) {
         console.error(e);
     }
+    // try {
+    //     const counter = db.collection(COUNTER);
+    //     const posts = db.collection(TASKS);
+
+    //     let query = { name: 'Total Post' };
+    //     let res = await counter.findOne(query);
+    //     console.log(res);
+    //     const totalPost = res.totalPost;
+
+    //     query = { _id: totalPost + 1, title: req.body.title, date: req.body.date };
+    //     res = await posts.insertOne(query);
+
+    //     query = { name: 'Total Post' };
+    //     let stage = { $inc: { totalPost: 1 } };
+    //     await counter.updateOne(query, stage);
+    // } catch (e) {
+    //     console.error(e);
+    // }
 }
 
-app.get('/list', function (req, resp) {
-    runListGet(req, resp);
+app.get('/list', async function (req, res, next) {
+    try {
+        //Finds all tasks currently stored in the database
+        const tasks = await taskModel.find().exec();
+        //Creates query out of tasklist defined above
+        const query = { posts: tasks };
+        //renders the list page using the tasks above
+        res.render('list.ejs', query);
+    } catch (error) {
+        //display the error
+        console.error(error);
+        //send the error as a response
+        res.status(500).send({ error: 'Error getting tasks' });
+    }
 });
 
-async function runListGet(req, resp) {
-    try {
-        const posts = db.collection(POSTS);
-        const res = await posts.find().toArray();
-        const query = { posts: res };
-        resp.render('list.ejs', query)
-    } catch (e) {
-        console.error(e);
-    }
-}
-
+//IMPORTANT: *I have no idea what the point of this function is
 app.get('/listjson', async function (req, resp) {
     try {
-        const posts = db.collection(POSTS);
+        const posts = db.collection(TASKS);
         const res = await posts.find().toArray();
         resp.send(res)
     } catch (e) {
@@ -94,66 +108,91 @@ app.get('/listjson', async function (req, resp) {
 });
 
 app.delete('/delete', async function (req, resp) {
-    req.body._id = parseInt(req.body._id); // the body._id is stored in string, so change it into an int value
-    console.log(req.body._id);
     try {
-        const counter = db.collection(COUNTER);
-        const posts = db.collection(POSTS)
-        const res = await posts.deleteOne(req.body);
-
-        const query = { name: 'Total Post' };
-        const stage = { $inc: { totalPost: -1 } };
-        await counter.updateOne(query, stage);
-
-        console.log('Delete complete')
-        resp.send('Delete complete')
+        const taskID = await (req.body._id)
+        console.log(req.body._id)
+        await taskModel.findOneAndDelete({ _id: taskID })
+        console.log("Successfully deleted task")
+    } catch (e){
+        console.error(e)
+        resp.status(500).send({ error: 'Error deleting task' });
     }
-    catch (e) {
-        console.error(e);
-    }
+    // req.body._id = parseInt(req.body._id); // the body._id is stored in string, so change it into an int value
+    // console.log(req.body._id);
+    // try {
+    //     const counter = db.collection(COUNTER);
+    //     const posts = db.collection(TASKS)
+    //     const res = await posts.deleteOne(req.body);
+
+    //     const query = { name: 'Total Post' };
+    //     const stage = { $inc: { totalPost: -1 } };
+    //     await counter.updateOne(query, stage);
+
+    //     console.log('Delete complete')
+    //     resp.send('Delete complete')
+    // }
+    // catch (e) {
+    //     console.error(e);
+    // }
 });
 
 app.get('/detail/:id', async function (req, resp) {
     try {
-        let res = await db.collection(POSTS).findOne({ _id: parseInt(req.params.id) })
-        // req.params.id contains the value of id
-        console.log('app.get.detail: Update complete')
-        console.log({ data: res });
-        if (res != null) {
-            resp.render('detail.ejs', { data: res })
-        }
-        else {
-            console.log(error);
-            resp.status(500).send({ error: 'result is null' })
-        }
-    }
-    catch (error) {
-        console.log(error)
-        resp.status(500).send({ error: 'Error from db.collection().findOne()' })
+        const taskID = await (req.params.id)
+        let taskInfo = await taskModel.findOne({ _id: taskID })
+        resp.render('detail.ejs', {data : taskInfo});
+    } catch (e) {
+        console.log(error);
+        //send the error as a response
+        resp.status(500).send({ error: 'Error' });
     }
 })
 
-app.get('/edit/:id', async function (req, resp) {
-    console.log(req.params)
+//API that just renders the edit page for specific tasks
+app.get('/update/:id', async function (req, resp) {
     try {
-        let res = await db.collection(POSTS).findOne({ _id: parseInt(req.params.id) })
-        console.log({ data: res })
-        if (res != null) {
-            resp.render('edit.ejs', { data: res })
+        const taskID = (req.params.id)
+        const task = await taskModel.findOne({ _id: taskID })
+        //If task exists then render the webpage
+        if(task) {
+            //Render edit.js with task information
+            resp.render('edit.ejs', {data : task})
+            console.log("Post has been found and now is being displayed")
+        //If task doesn't exist then don't send an error
+        } else {
+            resp.status(404).send("Post doesn't exist");
         }
-        else {
-            resp.status(500).send({ error: 'result is null' })
-        }
-    }
-    catch (error) {
-        console.log(error)
-        resp.status(500).send({ error: 'Error from db.collection().findOne()' })
+    } catch (e) {
+        console.error(e)
+        resp.status(500).send({ error: 'Error Updating task information' });
     }
 });
 
-app.put('/edit', async function (req, resp) {
-    db.collection(POSTS).updateOne({ _id: parseInt(req.body.id) }, { $set: { title: req.body.title, date: req.body.date } }, function () {
-        console.log('app.put.edit: Update complete')
-        resp.redirect('/list')
-    });
+//Update call that edits information
+//update a task information and send it to the database
+app.put('/update/:id', async function (req, resp) {
+    try {
+        //Setting task parameters
+        const taskID = (req.params.id);
+        const updatedTask = req.body;
+        const options = { new: true };
+        const task = await taskModel.findOneAndUpdate({_id: taskID}, updatedTask, options);
+
+        if (task) {
+            //Finds all tasks currently stored in the database
+            const tasks = await taskModel.find().exec();
+            //Creates query out of tasklist defined above
+            const query = { posts: tasks };
+            //renders the list page using the tasks above
+            resp.render('list.ejs', query);
+        } else {
+            //sends the error as a response
+            resp.status(404).send({ error: `Error Updating task information` });
+        }
+    } catch (error) {
+        //display the error
+        console.log(error);
+        //send the error as a response
+        resp.status(500).send({ error: 'Error updating post' });
+    }
 });
