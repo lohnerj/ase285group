88b77d798236d1e9a71c87cfb253166b7bd9cb26
Beanvia <hunter@giordullo.com>
2023-04-22 15:34:09 -0400

    Added Tests
    Added Tests

---
+++ darkmode.test.js
@@ -0,0 +1,9 @@
+const { setDark } = require('./models/models.ejs');
+const mongoose = require("mongoose");
+
+describe('Dark Mode Test', () => {
+	test('Should toggle the dark mode on/off', () => {
+		expect(setDark).toBe();
+		expect(setDark).toBe();
+	})
+});
