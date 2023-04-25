862d183be7a2b9bb16e44d575c48d4411fa4dab6
HightMaster <89708788+HightMaster@users.noreply.github.com>
2023-04-24 23:19:07 -0400

    Tests separated to two files

--- __tests__/favorite.test.ts
+++ __tests__/favorite.test.ts
@@ -15,8 +15,3 @@ describe('Test the updateFavorite API', () => {
     })
 });
 
-describe("Test the tomorrow API page", () => {
-    it("Given the Tomorrow API route, a 200 status code should be returned", () => {
-        supertest(app).get(`/tomorrow`).expect(200)
-    })
-})
---
+++ __tests__/tomorrow.test.ts
@@ -0,0 +1,12 @@
+const supertest = require("supertest");
+//import {app} from '..'
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+
+//JEST Test to test the tomorrow API page
+describe("Test the tomorrow API page", () => {
+    it("Given the Tomorrow API route, a 200 status code should be returned", () => {
+        supertest(app).get(`/tomorrow`).expect(200)
+    })
+})
